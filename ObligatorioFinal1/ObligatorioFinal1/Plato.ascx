c5eddb09847be33b80b72047e61305c0410e50b5
Rodrigo Antognazza <rantognazza@gmail.com>
2017-01-20 10:03:28 -0300

    UserControl-Plato

---
+++ ObligatorioFinal1/ObligatorioFinal1/Plato.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Plato.ascx.cs" Inherits="ObligatorioFinal1.Plato" %>
+<asp:Image ID="Image1" runat="server" Height="365px" Width="451px" />
+
---
+++ ObligatorioFinal1/ObligatorioFinal1/Plato.ascx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ObligatorioFinal1
+{
+    public partial class Plato : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
