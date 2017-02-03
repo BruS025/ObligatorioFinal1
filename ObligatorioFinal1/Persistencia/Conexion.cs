005cfc38950e4ee81235bb174e3009eb226dd0b0
ValeRivero <riverovaleria5@gmail.com>
2017-02-02 23:53:39 -0300

    Logica-Persistencia

---
+++ ObligatorioFinal1/Persistencia/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class Conexion
+    {
+        //Conexion A
+        //private static string cnnString = "Data Source=.;Initial Catalog = OBLIGATORIO; Integrated Security= true;";
+
+        //Conexion B
+        private static string cnnString = "Data Source=.\\SQLEXPRESS;Initial Catalog = OBLIGATORIO; Integrated Security= true;Connection Timeout=3500";
+
+        public static string CnnString
+        {
+            get
+            {
+                return cnnString;
+            }
+        }
+    }
+}
