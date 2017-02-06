5bbf3adf83e4d28f402a09331fda911467c2ecde
ValeRivero <riverovaleria5@gmail.com>
2017-02-05 21:28:50 -0300

    Casas-Agregar

--- ObligatorioFinal1/Persistencia/Conexion.cs
+++ ObligatorioFinal1/Persistencia/Conexion.cs
@@ -9,10 +9,10 @@ namespace Persistencia
     public class Conexion
     {
         //Conexion A
-        //private static string cnnString = "Data Source=.;Initial Catalog = OBLIGATORIO; Integrated Security= true;";
+        //private static string cnnString = "Data Source=.;Initial Catalog = OBLIGATORIOFINAL; Integrated Security= true;";
 
         //Conexion B
-        private static string cnnString = "Data Source=.\\SQLEXPRESS;Initial Catalog = OBLIGATORIO; Integrated Security= true;Connection Timeout=3500";
+        private static string cnnString = "Data Source=.\\SQLEXPRESS;Initial Catalog = OBLIGATORIOFINAL; Integrated Security= true;Connection Timeout=3500";
 
         public static string CnnString
         {
